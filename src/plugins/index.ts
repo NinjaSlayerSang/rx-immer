70827ed12f37834a92051c46961fb11b70c5ae91
肖俊江-pn20090106 <xiaojunjiang@huize.com>
2022-03-23 15:41:49 +0800

    fix: diachrony plugin export

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -2,6 +2,8 @@ export { QueryPluginExt, default as queryPlugin } from './query-plugin';
 export { AffairPluginExt, default as affairPlugin } from './affair-plugin';
 export { HistoryPluginExt, default as historyPlugin } from './history-plugin';
 export {
+  Flow,
+  Diachrony,
   DiachronyPluginExt,
   default as diachronyPlugin,
 } from './diachrony-plugin';
